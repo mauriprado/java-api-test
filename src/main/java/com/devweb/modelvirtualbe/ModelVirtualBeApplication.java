5b962b4009e642624c52305b19c25a35e54f5164
mauriprado <104120014+mauriprado@users.noreply.github.com>
2022-06-05 11:00:51 -0500

    First commit
    First commit in this repository,

---
+++ src/main/java/com/devweb/modelvirtualbe/ModelVirtualBeApplication.java
@@ -0,0 +1,13 @@
+package com.devweb.modelvirtualbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelVirtualBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModelVirtualBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/devweb/modelvirtualbe/ModelVirtualBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devweb.modelvirtualbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModelVirtualBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
